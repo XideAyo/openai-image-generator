416dbd4cee22745cb05a7137678ed90d2d1e1448
akinwale ayomide <akinwaleayomide55@gmail.com>
2023-01-23 10:06:45 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+function App() {
+  const configuration = new Configuration({
+    apiKey: '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',
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: 'Say this is a test',
+      n: 1,
+      size: '1024x1024',
+    });
+
+    console.log(res.data.data[0].url);
+  };
+
+  return (
+    <div>
+      <button onClick={generateImage}>Generate an Image</button>
+    </div>
+  );
+}
+
+export default App;
